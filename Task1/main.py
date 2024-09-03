145f1865d0b759b2a32a301af237448056c31bd1
Areesha Mahmood <areesha.mahmood@emumba.com>
2024-09-03 22:50:31 +0500

    Code Update: Fixed the 'Trailing Comma' Issue and 'Range' Issue

--- Task1/main.py
+++ Task1/main.py
@@ -2,9 +2,16 @@
 This script finds all numbers between 2000 and 3200 (inclusive) that are divisible by 7 but not multiples of 5.
 The resulting numbers are printed in a comma-separated sequence on a single line.
 """
+
+# List to store the numbers in
+num = []
+
 # Iterate through the specified range
-for i in range(2000,3200):
+for i in range(2000,3201):
     # If the number is divisible by 7 and not a multiple of 5
     if i%7 == 0 and i%5 != 0:
-        # Print the numbers in one line separated by a comma
-        print(i, end=", ")
+        # Append each number to the list
+        num.append(i)
+
+# Print the list in a single line, with each element separated by a comma
+print(*num, sep=", ")
