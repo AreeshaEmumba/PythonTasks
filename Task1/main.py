c14ebacedb126358637c6bac385bb7e516b182ef
Areesha Mahmood <areesha.mahmood@emumba.com>
2024-09-03 22:16:42 +0500

    Add doc string to the code

--- Task1/main.py
+++ Task1/main.py
@@ -1,11 +1,17 @@
-#list to store the numbers in
-num = [] 
+"""
+This script finds all numbers between 2000 and 3200 (inclusive) that are divisible by 7 but not multiples of 5.
+The resulting numbers are printed in a comma-separated sequence on a single line.
+"""
 
-#Iterate through the specified range
+# List to store the numbers in
+num = []
+
+# Iterate through the specified range
 for i in range(2000,3200):
-    #If the number is divisible by 7 and not a multiple of 5
+    # If the number is divisible by 7 and not a multiple of 5
     if i%7 == 0 and i%5 != 0:
-        #the number is converted to a string and added to the "num" list
+        # The number is converted to a string and added to the "num" list
         num.append(str(i))
-#join the list into a comma separated string and print it
+
+# Join the list into a comma separated string and print it
 print(", ".join(num))
