d5850547e66433effbe18afa132c19265165806c
Areesha Mahmood <areesha.mahmood@emumba.com>
2024-09-03 22:37:21 +0500

    COde UPdate: Instead of appending each numnumbers to a list, it prints them directly, separated by a comma

--- Task1/main.py
+++ Task1/main.py
@@ -2,16 +2,9 @@
 This script finds all numbers between 2000 and 3200 (inclusive) that are divisible by 7 but not multiples of 5.
 The resulting numbers are printed in a comma-separated sequence on a single line.
 """
-
-# List to store the numbers in
-num = []
-
 # Iterate through the specified range
 for i in range(2000,3200):
     # If the number is divisible by 7 and not a multiple of 5
     if i%7 == 0 and i%5 != 0:
-        # The number is converted to a string and added to the "num" list
-        num.append(str(i))
-
-# Join the list into a comma separated string and print it
-print(", ".join(num))
+        # Print the numbers in one line separated by a comma
+        print(i, end=", ")
