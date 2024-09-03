61bbe43922a6b317891da5a4fd6a6048bb045447
Areesha Mahmood <areesha.mahmood@emumba.com>
2024-09-03 22:05:56 +0500

    Add comments to the code

--- Task1/main.py
+++ Task1/main.py
@@ -1,5 +1,11 @@
-num = []
+#list to store the numbers in
+num = [] 
+
+#Iterate through the specified range
 for i in range(2000,3200):
+    #If the number is divisible by 7 and not a multiple of 5
     if i%7 == 0 and i%5 != 0:
+        #the number is converted to a string and added to the "num" list
         num.append(str(i))
+#join the list into a comma separated string and print it
 print(", ".join(num))
