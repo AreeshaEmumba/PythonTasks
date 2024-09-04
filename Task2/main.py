c69635abffef51a50cea3850dbd400622f454170
Areesha Mahmood <areesha.mahmood@emumba.com>
2024-09-04 17:47:32 +0500

    Code Update: Handling of incorrect input along with a guide to enter the correct inputs

--- Task2/main.py
+++ Task2/main.py
@@ -9,9 +9,13 @@ parser.add_argument("--Left", type=int, default=0, help="Distance moved left")
 
 arguments = parser.parse_args()
 
-def calculate_distance(u,d,r,l):
-    vertical = u - d
-    horizontal = r - l
+if arguments.Up < 0 or arguments.Down < 0 or arguments.Right < 0 or arguments.Left < 0:
+    print("\n Error! \n All inputs must be non-negative integers. \n Usage Emaple: \t --Up 10 --Down 20 --Right 0 --Left 3")
+    exit(1)
+
+def calculate_distance(up,down,right,left):
+    vertical = up - down
+    horizontal = right - left
 
     dist = math.sqrt(vertical**2 + horizontal**2)
     distance = round(dist)
